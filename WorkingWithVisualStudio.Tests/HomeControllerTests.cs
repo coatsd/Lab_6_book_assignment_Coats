6990aed2513829d3153307978f18ab8e076d1e63
Daniel Coats <coatsd@my.lanecc.edu>
2018-11-14 15:36:43 -0800

    Created Comparer Class and HomeController Tests

--- WorkingWithVisualStudio.Tests/HomeControllerTests.cs
+++ WorkingWithVisualStudio.Tests/HomeControllerTests.cs
@@ -1,11 +1,26 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using Microsoft.AspNetCore.Mvc;
+using WorkingWithVisualStudio.Controllers;
+using WorkingWithVisualStudio.Models;
+using Xunit;
 
 namespace WorkingWithVisualStudio.Tests
 {
-    class HomeControllerTests
+    public class HomeControllerTests
     {
+        [Fact]
+        public void IndexActionModelIsComplete()
+        {
+            var controller = new HomeController();
 
+            var model = (controller.Index() as ViewResult)?.ViewData.Model 
+                as IEnumerable<Product>;
+
+            Assert.Equal(SimpleRepository.SharedRepository.Products, model,
+                Comparer.Get<Product>((p1, p2) => p1.Name == p2.Name &&
+                    p1.Price == p2.Price));
+        }
     }
 }
